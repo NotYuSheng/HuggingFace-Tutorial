9a905800585a3f3bfab8946766d3c1eb99e6dbb9
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-06-05 10:41:49 +0800

    Update speech-recognizer.py

--- speech-recognizer.py
+++ speech-recognizer.py
@@ -4,7 +4,6 @@ from datasets import load_dataset, Audio
 
 speech_recognizer = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
 
-
 dataset = load_dataset("PolyAI/minds14", name="en-US", split="train")
 dataset = dataset.cast_column("audio", Audio(sampling_rate=speech_recognizer.feature_extractor.sampling_rate))
 
