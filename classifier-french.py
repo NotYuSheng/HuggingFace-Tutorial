15f5376be397dde14d676533cdb3292cdf394761
Yu Sheng <37260088+NotYuSheng@users.noreply.github.com>
2024-06-04 22:31:00 -0400

    Add files via upload

---
+++ classifier-french.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+results = classifier("Nous sommes très heureux de vous présenter la bibliothèque 🤗 Transformers.")
+
+for result in results:
+    print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
